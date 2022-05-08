2f1529a9736095590d3f1f221799860d0b4ec611
Faizul-Hassan-2015 <86608620+Faizul-Hassan-2015@users.noreply.github.com>
2022-05-08 20:47:34 +0600

    add database and send response

--- index.js
+++ index.js
@@ -11,14 +11,29 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://dbuser1:<password>@cluster0.afo5v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSCODE}@cluster0.afo5v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+        try{
+            await client.connect();
+            const equiptmentCollection = client.db('football-universe').collection('equipment');
+            
+            app.get('/equipment', async(req,res)=>{
+            const query = {};
+            const cursor = equiptmentCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+            })
+        }
+
+        finally{
+
+        }
+}
+
+
+run().catch(console.dir);
 
 app.get('/', (req, res) =>{
     res.send('football universe server')
